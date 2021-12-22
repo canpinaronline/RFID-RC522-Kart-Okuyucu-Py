73a16484909795cc93c0e04780e8fddba3cf3ce9
canpinaronline <64422728+canpinaronline@users.noreply.github.com>
2021-12-22 16:02:18 +0300

    Add files via upload

---
+++ kart_kayit.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import time
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+import mysql.connector
+import Adafruit_CharLCD as LCD
+
+db = mysql.connector.connect(
+  host="localhost",
+  user="attendanceadmin",
+  passwd="pimylifeup",
+  database="attendancesystem"
+)
+
+cursor = db.cursor()
+reader = SimpleMFRC522()
+lcd = LCD.Adafruit_CharLCD(4, 24, 23, 17, 18, 22, 16, 2, 4);
+
+try:
+  while True:
+    lcd.clear()
+    lcd.message('Place to\nregister')
+    id, text = reader.read()
+    cursor.execute("SELECT id FROM users WHERE rfid_uid="+str(id))
+    cursor.fetchone()
+
+    if cursor.rowcount >= 1:
+      lcd.clear()
+      lcd.message("Overwrite\nexisting user?")
+      overwrite = input("Overwite (Y/N)? ")
+      if overwrite[0] == 'Y' or overwrite[0] == 'y':
+        lcd.clear()
+        lcd.message("Overwriting user.")
+        time.sleep(1)
+        sql_insert = "UPDATE users SET name = %s, ogr_sin = %s, ogr_bol = %s  WHERE rfid_uid=%s"
+      else:
+        continue;
+    else:
+      sql_insert = "INSERT INTO users (name, rfid_uid, ogr_sin, ogr_bol) VALUES (%s, %s, %s, %s)" #Veritabanındaki kolon isimleri buraya yazılacak.
+    lcd.clear()
+    lcd.message('Isim giriniz: ')
+    new_name = input("Isim: ")
+    lcd.clear()
+    lcd.message("Sinif giriniz: ")
+    ogrenci_sin = input("Sınıf: ")
+    lcd.clear()
+    ogrenci_bol = input("Bolum giriniz: ")
+
+    cursor.execute(sql_insert, (new_name, ogrenci_sin, ogrenci_bol, id))
+
+    db.commit()
+
+    lcd.clear()
+    lcd.message("Ogrenci " + new_name + "\nkaydedildi.")
+    time.sleep(2)
+finally:
+  GPIO.cleanup()
+
---
+++ yoklama_kayit.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import time
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+import mysql.connector
+import Adafruit_CharLCD as LCD
+
+db = mysql.connector.connect(
+  host="localhost",
+  user="attendanceadmin",
+  passwd="pimylifeup",
+  database="attendancesystem"
+)
+
+cursor = db.cursor()
+reader = SimpleMFRC522()
+
+lcd = LCD.Adafruit_CharLCD(4, 24, 23, 17, 18, 22, 16, 2, 4);
+
+try:
+  while True:
+    lcd.clear()
+    lcd.message('Yoklama icin\nkarti okutun')
+    id, text = reader.read()
+
+    cursor.execute("Select id, name FROM users WHERE rfid_uid="+str(id))
+    result = cursor.fetchone()
+
+    lcd.clear()
+
+    if cursor.rowcount >= 1:
+      lcd.message("Hosgeldin " + result[1])
+      cursor.execute("INSERT INTO attendance (user_id) VALUES (%s)", (result[0],) )
+      db.commit()
+    else:
+      lcd.message("Kullanici bulunamadi.")
+    time.sleep(2)
+finally:
+  GPIO.cleanup()
+
