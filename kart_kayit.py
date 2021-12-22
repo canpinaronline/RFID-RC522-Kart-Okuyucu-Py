6beda37de3dd9a7dfc5fa798789b9cadc3985e2b
canpinaronline <64422728+canpinaronline@users.noreply.github.com>
2021-12-22 21:32:56 +0300

    Update kart_kayit.py

--- kart_kayit.py
+++ kart_kayit.py
@@ -20,18 +20,18 @@ lcd = LCD.Adafruit_CharLCD(4, 24, 23, 17, 18, 22, 16, 2, 4);
 try:
   while True:
     lcd.clear()
-    lcd.message('Place to\nregister')
+    lcd.message('Kayit icin\nkarti gosteriniz.')
     id, text = reader.read()
     cursor.execute("SELECT id FROM users WHERE rfid_uid="+str(id))
     cursor.fetchone()
 
     if cursor.rowcount >= 1:
       lcd.clear()
-      lcd.message("Overwrite\nexisting user?")
-      overwrite = input("Overwite (Y/N)? ")
+      lcd.message("Mevcut kartın ustune yaz?")
+      overwrite = input("Onay: (Y/N)? ")
       if overwrite[0] == 'Y' or overwrite[0] == 'y':
         lcd.clear()
-        lcd.message("Overwriting user.")
+        lcd.message("Ustune yaziliyor.")
         time.sleep(1)
         sql_insert = "UPDATE users SET name = %s, ogr_sin = %s, ogr_bol = %s  WHERE rfid_uid=%s"
       else:
